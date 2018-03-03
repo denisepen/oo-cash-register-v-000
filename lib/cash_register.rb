b18b11ffb5cac23c1fab0adfe7c6d31f4a102ed0
Denise Pendleton <dpendl1@hotmail.com>
2018-03-03 22:33:01 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 
 class CashRegister
-    attr_accessor :discount, :total, :quantity, :price, :total
+    attr_accessor :discount, :total, :quantity, :price
 
   def initialize(discount = 0)
     @total = 0
